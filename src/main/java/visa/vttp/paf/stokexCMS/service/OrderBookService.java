abe9cb8a1f79bf46478d3106b2a6255e801bfacd
Alfred A <20140832+alfredats@users.noreply.github.com>
2022-05-16 20:58:09 +0800

    Clean up orderBook repo, service and tests

--- src/main/java/visa/vttp/paf/stokexCMS/service/OrderBookService.java
+++ src/main/java/visa/vttp/paf/stokexCMS/service/OrderBookService.java
@@ -1,63 +1,28 @@
 package visa.vttp.paf.stokexCMS.service;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import javax.annotation.PostConstruct;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import visa.vttp.paf.stokexCMS.constants.OrderStatus;
-import visa.vttp.paf.stokexCMS.constants.OrderType;
-import visa.vttp.paf.stokexCMS.engine.OrderBookEngine;
-import static visa.vttp.paf.stokexCMS.engine.OrderBookEngine.TICKER_TEST_ONLY;
-import visa.vttp.paf.stokexCMS.engine.datatypes.Executed;
-import visa.vttp.paf.stokexCMS.engine.datatypes.ExecutedCancel;
-import visa.vttp.paf.stokexCMS.engine.datatypes.ExecutedTrade;
 import visa.vttp.paf.stokexCMS.model.Order;
 import visa.vttp.paf.stokexCMS.repo.OrderBookRepository;
-import visa.vttp.paf.stokexCMS.repo.TradesRepository;
 
 
 @Service
 public class OrderBookService {
-    private static final Logger logger = LoggerFactory.getLogger(OrderBookService.class);
 
     @Autowired
     private OrderBookRepository obRepo;
 
-    @Autowired 
-    private TradesRepository tRepo;
-
-    @Autowired 
-    private static Map<String, OrderBookEngine> obEngineMap = new HashMap<>();
-
-    @PostConstruct
-    private static void init() {
-        /**
-         * INIT INSTANCES OF ORDERBOOKENGINE FOR EACH TICKER
-         */
-        obEngineMap.put(TICKER_TEST_ONLY, OrderBookEngine.createOrderBookEngine(TICKER_TEST_ONLY));
-        obEngineMap.put("AAPL", OrderBookEngine.createOrderBookEngine("AAPL"));
-    }
-
-
-    public Integer submitOrder(Order o) {
-        /**
-         * 1) submit order to mysql as backup
-         * 2) send order to appropriate orderbookengine
-         */
+    public Integer submitNewOrder(Order o) {
         Integer id = o.getOrderID();
-        if (null == id) {
-            id = obRepo.submitOrder(o);
-            o.setOrderID(id);
+        if (null != id) {
+            throw new RuntimeException("submitNewOrder received order with orderID: %s, cancelling submission...".formatted(id));
         }
-        obEngineMap.get(o.getTicker()).processOrder(o);
+        id = obRepo.submitOrder(o);
+        o.setOrderID(id);
         return id;
     }
 
@@ -73,37 +38,13 @@ public class OrderBookService {
         }
     }
 
-    public void syncEngines() {
-        for (String ticker : obEngineMap.keySet()) {
-            this.syncProcessed(ticker);
-        }
+    public List<Order> getOrdersByUsername(String username, Boolean isActive) {
+        if (isActive) { return obRepo.getActiveOrdersByUsername(username); }
+        return obRepo.getAllOrdersByUsername(username);
     }
 
-    @Transactional
-    public void syncProcessed(String ticker) {
-        OrderBookEngine obe = obEngineMap.get(ticker);
-        List<Executed> ops = obe.getExecutedOps();
-        try {
-            ops.stream()
-                .filter(x -> (x != null))
-                .forEach((Executed e) -> {
-                    if (e instanceof ExecutedTrade) {
-                        ExecutedTrade t = (ExecutedTrade) e;
-                        this.updateOrderStatus(t.getBid());
-                        this.updateOrderStatus(t.getAsk());
-                        tRepo.submitTrade(t);                
-                    } else if (e instanceof ExecutedCancel) {
-                        ExecutedCancel c = (ExecutedCancel) e;
-                        this.updateOrderStatus(c.getCancelledOrder());
-                    }
-                });
-        } catch (RuntimeException ex) {
-            logger.error(ex.getStackTrace().toString());
-            logger.info(">>> FAILED TO SYNC TRADES FROM %s ENGINE".formatted(ticker));
-            throw new RuntimeException();
-        }
+    public List<Order> getActiveOrders() {
+        return obRepo.getActiveOrders();
     }
 
-    
-    
 }
--- src/test/java/visa/vttp/paf/stokexCMS/Test_OrderBookService.java
+++ src/test/java/visa/vttp/paf/stokexCMS/Test_OrderBookService.java
@@ -1,34 +1,32 @@
 package visa.vttp.paf.stokexCMS;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import visa.vttp.paf.stokexCMS.constants.OrderStatus;
 import visa.vttp.paf.stokexCMS.constants.OrderType;
+import visa.vttp.paf.stokexCMS.engine.OrderBookEngine;
 import visa.vttp.paf.stokexCMS.model.Order;
 import visa.vttp.paf.stokexCMS.repo.OrderBookRepository;
+import visa.vttp.paf.stokexCMS.repo.TradesRepository;
 import visa.vttp.paf.stokexCMS.service.OrderBookService;
 import static visa.vttp.paf.stokexCMS.engine.OrderBookEngine.TICKER_TEST_ONLY;
 
 import java.time.LocalDateTime;
 
 @SpringBootTest
+@TestInstance(Lifecycle.PER_CLASS)
 class Test_OrderBookService {
-
-    @Test
-    public void test() {
-        assertTrue(true);
-    }
 
     @Autowired
     private OrderBookService obSvc;
@@ -36,10 +34,13 @@ class Test_OrderBookService {
     @Autowired 
     private OrderBookRepository obRepo;
 
+    @Autowired
+    private TradesRepository tRepo;
+
     private Order testAsk;
     private Order testBid;
 
-    @BeforeEach
+    @BeforeAll
     private void initTestOrders() {
         this.testBid = new Order();
         this.testBid.setOrderType(OrderType.bid);
@@ -58,52 +59,58 @@ class Test_OrderBookService {
         this.testAsk.setPrice(100.00d);
         this.testAsk.setUsername("testUser2");
         this.testAsk.setCreated(LocalDateTime.now());
-    
-        obRepo.submitOrder(this.testBid);
-        obRepo.submitOrder(this.testAsk);
+    }
+
+    @BeforeEach
+    private void removeOrderIDs() {
+        this.testBid.setOrderID(null);
+        this.testAsk.setOrderID(null);
     }
     
     @AfterEach
     private void deleteTestOrders() {
+        tRepo.deleteTradesByTicker(TICKER_TEST_ONLY);
         obRepo.deleteOrdersByTicker(TICKER_TEST_ONLY);
     }
 
 
     @Test
-    public void submitOrderSuccessful() {
-        assertTrue(null != obSvc.submitOrder(this.testBid));
-        assertTrue(null != obSvc.submitOrder(this.testAsk));
+    @org.junit.jupiter.api.Order(1)
+    public void submitNewOrderSuccessful() {
+        assertTrue(null != obSvc.submitNewOrder(this.testBid));
+        assertTrue(null != obSvc.submitNewOrder(this.testAsk));
     }
 
     @Test
+    @org.junit.jupiter.api.Order(2)
     public void failInvalidOrderType() {
         Order invalidType = this.testAsk;
+        Integer prev_id = invalidType.getOrderID();
+        invalidType.setOrderID(null);
         invalidType.setOrderType(-1);
-        assertThrows(RuntimeException.class, () -> { obSvc.submitOrder(invalidType); });
+        assertThrows(RuntimeException.class, () -> { obSvc.submitNewOrder(invalidType); });
     }
 
     @Test
-    public void submitCancelOrderSuccessful() {
-        Integer id = obSvc.submitOrder(this.testAsk);
-        assertTrue(null != id, "Failed to submit preamble test record for cancellation");
-        this.testAsk.setOrderStatus(OrderStatus.cancelled);
-        this.testAsk.setUpdated(LocalDateTime.now());
-        try {
-            obSvc.submitOrder(this.testAsk);
-        } catch (RuntimeException ex) {
-            fail("Failed to process order of type cancelled");
+    @org.junit.jupiter.api.Order(3)
+    public void updateFulfilledOrderSuccessful() {
+        Integer id = obSvc.submitNewOrder(this.testBid);
+        this.testBid.setOrderStatus(OrderStatus.fulfilled);
+        assertTrue(obSvc.updateOrderStatus(this.testBid));
+        Order o = obRepo.getOrderByOrderID(id);
+        if (!OrderStatus.fulfilled.equals(o.getOrderStatus()) || !o.getUnfulfilledQty().equals(100)) {
+            fail("SQL does not persist with fulfillment info for order of ID %d".formatted(id));
         }
         assertTrue(true);
     }
 
     @Test 
-    public void updateOrderStatusSuccessful() {
-        Integer id = obSvc.submitOrder(this.testAsk);
+    @org.junit.jupiter.api.Order(4)
+    public void updatePartialOrderSuccessful() {
+        Integer id = obSvc.submitNewOrder(this.testAsk);
         this.testAsk.setOrderStatus(OrderStatus.partiallyFulfilled);
         this.testAsk.setUnfulfilledQty(50);
-        if (obSvc.updateOrderStatus(this.testAsk) != true) {
-            fail("Failed to update order of ID %d with partial fulfillment info;".formatted(id));
-        }
+        assertTrue(obSvc.updateOrderStatus(this.testAsk));
         Order o = obRepo.getOrderByOrderID(id);
         if (!OrderStatus.partiallyFulfilled.equals(o.getOrderStatus()) || !o.getUnfulfilledQty().equals(50)) {
             fail("SQL does not persist with partial fulfillment info for order of ID %d".formatted(id));
@@ -111,5 +118,19 @@ class Test_OrderBookService {
         assertTrue(true);
     }
 
+    @Test
+    @org.junit.jupiter.api.Order(5)
+    public void updateCancelOrderSuccessful() {
+        Integer id = obSvc.submitNewOrder(this.testAsk);
+        assertTrue(null != id, "Failed to submit preamble test record for cancellation");
+        this.testAsk.setOrderStatus(OrderStatus.cancelled);
+        this.testAsk.setUpdated(LocalDateTime.now());
+        try {
+            obSvc.updateOrderStatus(this.testAsk);
+        } catch (RuntimeException ex) {
+            fail("Failed to process order of type cancelled");
+        }
+        assertTrue(true);
+    }
 
 }
