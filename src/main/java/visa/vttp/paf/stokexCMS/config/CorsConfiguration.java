7ad06f5f454e22e32bca57b1bf44f8bccf3f2e12
Alfred A <20140832+alfredats@users.noreply.github.com>
2022-05-21 23:18:52 +0800

    add CORS policy, and heroku build instructions

---
+++ src/main/java/visa/vttp/paf/stokexCMS/config/CorsConfiguration.java
@@ -0,0 +1,17 @@
+package visa.vttp.paf.stokexCMS.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+    
+}
